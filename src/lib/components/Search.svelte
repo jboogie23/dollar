2ea7478f8e953550681910e721097a65b19b312b
Ross <ross.h.sanderson@gmail.com>
2024-02-23 19:36:44 +1100

    finished lesson 15 for responsiveness

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -10,8 +10,8 @@
 		type="text"
 		name="search"
 		placeholder="Search by keyword"
-		class="w-72 border-b-2 border-dashed border-b-pastelPurple bg-transparent font-sansSerif text-xl text-black outline-none focus:border-solid focus:border-b-lavenderIndigo"
+		class="w-full border-b-2 border-dashed border-b-pastelPurple bg-transparent font-sansSerif text-base text-black outline-none focus:border-solid focus:border-b-lavenderIndigo md:w-[14rem] lg:w-72 lg:text-xl"
 		autocomplete="off"
 	/>
-	<button class="font-sansSerif text-xl font-black text-pastelPurple">Search</button>
+	<button class="font-sansSerif text-lg font-black text-pastelPurple lg:text-xl">Search</button>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,29 +16,29 @@ export default {
 			},
 			boxShadow: {
 				colored: '0px 7px 16px rgba(127, 91, 220, 0.002)',
-				coloredHover: '0px 7px 16px rgba(127, 91, 220, 0.3)',
-				tableRow: '0px 0px 6px rgba(0,0,0,0.16)'
+				coloredHover: '0px 16px 25px rgba(127, 91, 220, 0.3)',
+				tableRow: '0px 0px 6px rgba(0, 0, 0, 0.16)'
 			},
 			colors: {
-				blueGem: '#3813a0',
-				caribbeanGreen: '#00ca9e',
-				caribbeanGreen: '#1CC6A0',
-				daisyBush: '#4714a5',
-				fog: '#decaec',
-				gallery: '#efeaf2',
-				goldenFizz: '#feff40',
 				lavenderIndigo: '#8657e1',
-				monsoon: '#777878',
+				daisyBush: '#4714a5',
+				goldenFizz: '#feff40',
+				whisper: '#f9f4f9',
 				pastelPurple: '#b2a1bb',
-				prim: '#ede6ef',
 				purple: '#7209b7',
 				robinEggBlue: '#00e9c0',
+				blueGem: '#3813a0',
+				caribbeanGreen: '#1CC6A0',
 				scarlet: '#f72f45',
+				monsoon: '#777878',
 				silver: '#c0c0c0',
-				whisper: '#f9f4f9'
+				gallery: '#efeaf2',
+				caribbeanGreen: '#00ca9e',
+				fog: '#decaec'
 			},
 			gridTemplateColumns: {
-				invoiceTable: '100px 100px 60px 1fr 116px 32px 32px'
+				invoiceTable: '100px 100px 60px 1fr 116px 32px 32px',
+				invoiceTableMobile: '1fr 90px'
 			},
 			zIndex: {
 				navBarToggle: 91,
